b1612985c1ede23173c5051cc609f6c50130b9b5
Raymundo Vásquez Ruiz <raymundo.vr@protonmail.com>
2023-02-23 20:00:38 +0100

    topic queues

---
+++ src/bin/topic_consumer.rs
@@ -0,0 +1,42 @@
+use amiquip::{ExchangeType, FieldTable, ConsumerOptions, ConsumerMessage};
+use anyhow::Result;
+use basic_amqp::{amqp_utils::get_connection, configuration};
+
+fn main() -> Result<()> {
+    let default_url = "amqp://localhost:15672".to_string();
+    let topic = "app.user.*";
+    let exchange_name = "exchange.app_user";
+
+    let config = configuration::load()?;
+    let amqp_url = config.get("amqp_url").unwrap_or(&default_url);
+
+    let mut connection = get_connection(&amqp_url)?;
+    let channel = connection.open_channel(None)?;
+    let exchange = channel.exchange_declare(
+        ExchangeType::Topic,
+        exchange_name,
+        amiquip::ExchangeDeclareOptions::default(),
+    )?;
+
+    let queue = channel.queue_declare("", amiquip::QueueDeclareOptions::default())?;
+    queue.bind(&exchange, topic, FieldTable::default())?;
+
+    println!("Listening to messages on Q {} for topic {topic}", queue.name());
+
+    let consumer = queue.consume(ConsumerOptions::default())?;
+    for (i, message) in consumer.receiver().iter().enumerate() {
+        match message {
+            ConsumerMessage::Delivery(payload) => {
+                let data = String::from_utf8_lossy(&payload.body);
+                println!("Received message {i}: {data}");
+            },
+            _ => {
+                eprintln!("Received a disconnect message!");
+            }
+        }
+    }
+
+    connection.close()?;
+
+    Ok(())
+}
---
+++ src/bin/topic_producer.rs
@@ -0,0 +1,29 @@
+use amiquip::Publish;
+use anyhow::Result;
+use basic_amqp::{configuration, amqp_utils::get_connection};
+
+fn main () -> Result<()> {
+    let default_url = "amqp://localhost:15672".to_string();
+    let created_topic = "app.user.created";
+    let deleted_topic = "app.user.deleted";
+    let exchange_name = "exchange.app_user";
+
+    let config = configuration::load()?;
+    let amqp_url = config.get("amqp_url").unwrap_or(&default_url);
+
+    let mut connection = get_connection(&amqp_url)?;
+    let channel = connection.open_channel(None)?;
+    let exchange = channel.exchange_declare_passive(exchange_name)?;
+
+    println!("Publish a delete message");
+    let message = r#"{"message": "User deleted"}"#.as_bytes();
+    exchange.publish(Publish::new(message, deleted_topic))?;
+
+    println!("Publish a create message");
+    let message = r#"{"message": "User create"}"#.as_bytes();
+    exchange.publish(Publish::new(message, created_topic))?;
+
+    println!("Done!");
+
+    Ok(())
+}
