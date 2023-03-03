3849836dbbb67a24e3a6ec5bf1254ace5f7e3c7a
Raymundo Vásquez Ruiz <raymundo.vr@protonmail.com>
2023-03-03 22:28:00 +0100

    chore: add all app topic consumer

--- src/bin/topic_producer.rs
+++ src/bin/topic_producer.rs
@@ -15,6 +15,7 @@ fn main () -> Result<()> {
     let channel = connection.open_channel(None)?;
     let exchange = channel.exchange_declare_passive(exchange_name)?;
 
+    // For all Qs interested in app.user
     println!("Publish a delete message");
     let message = r#"{"message": "User deleted"}"#.as_bytes();
     exchange.publish(Publish::new(message, deleted_topic))?;
@@ -22,6 +23,11 @@ fn main () -> Result<()> {
     println!("Publish a create message");
     let message = r#"{"message": "User create"}"#.as_bytes();
     exchange.publish(Publish::new(message, created_topic))?;
+
+    // For all app events Q. See: topic_consumer_allapp
+    println!("Publish a content created message");
+    let message = r#"{"content": "Hello, world!", "user": "a"}"#.as_bytes();
+    exchange.publish(Publish::new(message, "app.content.created"))?;
 
     println!("Done!");
 
