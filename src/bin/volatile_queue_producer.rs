b32c9a86c37449ffeb7f3c01d309b53cc749bb53
Raymundo Vásquez Ruiz <raymundo.vr@protonmail.com>
2023-03-03 21:46:00 +0100

    chore: add auto_delete queue

---
+++ src/bin/volatile_queue_producer.rs
@@ -0,0 +1,24 @@
+use amiquip::{Publish, AmqpProperties};
+use anyhow::Result;
+use basic_amqp::{configuration, amqp_utils};
+
+fn main() -> Result<()> {
+    let configuration = configuration::load()?;
+    let default_url = "amqp://localhost:15672".to_string();
+    let routing_key = "queue.app.volatile";
+
+    let amqp_url = configuration.get("amqp_url").unwrap_or(&default_url);
+
+    let mut connection = amqp_utils::get_connection(amqp_url)?;
+    let channel = connection.open_channel(None)?;
+    channel.basic_publish("", Publish::with_properties(
+        b"Hello, volatile!",
+        routing_key,
+        AmqpProperties::default().with_delivery_mode(2),
+    ))?;
+
+    connection.close()?;
+
+    println!("Done!");
+    Ok(())
+}
